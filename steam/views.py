3f9e63a5e5b8205bf31d7706349473e5ecdf652f
dsethan <ethan@dormserv.com>
2014-11-12 12:30:59 -0500

    ethan add user login

--- steam/views.py
+++ steam/views.py
@@ -4,15 +4,42 @@ from django.core.urlresolvers import reverse, reverse_lazy
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from steam.models import Profile
+from django.contrib.auth import login as user_login
+from django.contrib.auth import authenticate
 # , Personality, Question,Responses
 from django.forms.models import ModelForm, inlineformset_factory
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                user_login(request, user)
+                return HttpResponse("Successful login")
+
+            else:
+                return HttpResponse("Your account has been disabled")
+
+        else:
+            return render_to_response('steam/login.html',
+                {
+                'exists_error':True,
+                'error':"Please enter a valid username/password combination."
+                },
+                context_instance=RequestContext(request))
+
+
     return render_to_response('steam/login.html',
-        {},
-        context_instance=RequestContext(request))
+        {
+        'error':False,
+        },
+       context_instance=RequestContext(request))
 
 def logout(request):
     auth_logout(request)
